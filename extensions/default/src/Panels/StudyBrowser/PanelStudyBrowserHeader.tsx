d04692f2946679f08600bdf50216a4fd2877dd19
Joseph Maratt <20288451+jmaratt@users.noreply.github.com>
2024-12-26 08:06:11 -0500

    Hide settings in PanelStudyBrowserHeader

--- extensions/default/src/Panels/StudyBrowser/PanelStudyBrowserHeader.tsx
+++ extensions/default/src/Panels/StudyBrowser/PanelStudyBrowserHeader.tsx
@@ -20,7 +20,10 @@ function PanelStudyBrowserHeader({
         <div className={'flex h-[24px] w-full select-none justify-center self-center text-[14px]'}>
           <div className="flex w-full items-center gap-[10px]">
             <div className="flex items-center justify-center">
-              <div className="text-primary-active flex items-center space-x-1">
+              <div
+                className="text-primary-active flex items-center space-x-1"
+                style={{ display: 'none', visibility: 'hidden' }}
+              >
                 {actionIcons.map((icon: actionIcon, index) =>
                   React.createElement(Icons[icon.iconName] || Icons.MissingIcon, {
                     key: index,
